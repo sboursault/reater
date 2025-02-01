521ce165d4f4c80e6794f55b22d891fdb0d3070a
sbo <sbo@intractiv.com>
2025-02-01 06:51:08 +0100

    test tree in progress

--- src/app/_components/test-tree.tsx
+++ src/app/_components/test-tree.tsx
@@ -1,32 +1,58 @@
+"use client"
+import { ReactNode, useState } from "react"
+import TestGroup from "../../types/test-group"
 
-import { ReactNode } from "react"
-import TestGroup  from "../../types/test-group"
-
-export default function TestTree({ group }: {group: TestGroup}) {
-    return (
-        <>
-            { group.name? 
-                (
-                    <li>{group.name}
-                        <ListSubGroup group={group}></ListSubGroup>
-                    </li>
-                ) : (
-                    <ListSubGroup group={group}></ListSubGroup>
-                )
-            }
-        </>
-    )
+export default function TestTree({ group }: { group: TestGroup }) {
+  return (
+    <section className="menu">
+      {group.subGroups &&
+        (group.subGroups || []).map((subGroup) => (
+          <TopTree key={subGroup.name} group={subGroup}></TopTree>
+        ))}
+    </section>
+  )
 }
 
-function ListSubGroup({ group }: {group: TestGroup}):ReactNode {
-    return     (
-        <>
-        {group.subGroups &&  
-        <ul>
+function TopTree({ group }: { group: TestGroup }): ReactNode {
+  return (
+    <>
+      <p className="menu-label">{group.name}</p>
+      <ListSubGroup group={group} folded={false}></ListSubGroup>
+    </>
+  )
+}
+
+function ListSubGroup({ group, folded }: { group: TestGroup, folded: boolean }): ReactNode {
+  return (
+    <>
+      {group.subGroups &&
+        <ul className={"menu-list " + folded ? 'is-hidden' : ''}>
           {(group.subGroups || []).map((subGroup) => (
-          <TestTree key={subGroup.name} group={subGroup}></TestTree>
-      ))}
+            <SubTree key={subGroup.name} group={subGroup}></SubTree>
+          ))}
         </ul>}
-        </>
-      )
+    </>
+  )
+}
+
+
+export function SubTree({ group }: { group: TestGroup }) {
+
+  const [folded, setFolded] = useState(true);
+
+  const toggle = () => {
+    setFolded(false);
+  }
+
+  return (
+    <li>
+      <a>
+        <span className="icon" onClick={() => toggle()}>
+          <i className="fas fa-arrow-right"></i>
+        </span>
+        {group.name}
+      </a>
+      <ListSubGroup group={group} folded={folded}></ListSubGroup>
+    </li>
+  )
 }
--- src/services/test-report.ts
+++ src/services/test-report.ts
@@ -13,10 +13,38 @@ export default function getReport(): TestGroup {
                     {
                         name: "child 2-1",
                     },
+                    {
+                        name: "child 2-2",
+                        subGroups: [
+                            {
+                                name: "child 2-2-1",
+                            },
+                            {
+                                name: "child 2-2-2",
+                            },
+                            {
+                                name: "child 2-2-3",
+                            },
+                        ]
+                    },
+                    {
+                        name: "child 2-3",
+                    },
                 ]
             },
             {
                 name: "child 3",
+                subGroups: [
+                    {
+                        name: "child 3-1",
+                    },
+                    {
+                        name: "child 3-3",
+                    },
+                ]
+            },
+            {
+                name: "child 4",
             }
         ]
     }
