4c15b8c83b60d6545b1dba7f2756af9dec1f625d
sbo <sbo@intractiv.com>
2025-02-26 06:59:24 +0100

    select failed executions by default

--- src/app/_components/test-detail.tsx
+++ src/app/_components/test-detail.tsx
@@ -10,7 +10,7 @@ export default function TestDetail({
   data: Test,
   deactivateTest: () => void
 }) {
-  const [activeExecution, setActiveExecution] = useState<Execution>(data.executions[0]);
+  const [activeExecution, setActiveExecution] = useState<Execution>(getDefaultExecution(data));
 
   const tabs = data.executions.map((eachExecution, index) => {
     return (
@@ -38,6 +38,12 @@ export default function TestDetail({
     </article>
   )
 
+}
+
+function getDefaultExecution(data: Test): Execution {
+  const failedExecutions = data.executions.filter(each => each.status == Status.failed)
+  if (failedExecutions.length) return failedExecutions[0]
+  else return data.executions[0]
 }
 
 export function ExecutionTab({
@@ -69,25 +75,37 @@ export function ExecutionDetail({
 }: {
   data: Execution,
 }) {
+  return (
+    <div className="content">
+      {data.error &&
+        <div className="field">
+          <p><strong>Console log</strong></p>
+          <ConsoleLog data={data.error?.message}></ConsoleLog>
+        </div>
+      }
+    </div>
+  )
+}
 
-  const errorMessage = new AnsiUp().ansi_to_html(data.error?.message || '')
+export function ConsoleLog({
+  data,
+}: {
+  data: string,
+}) {
+  const html = new AnsiUp().ansi_to_html(data)
     .replaceAll(/\n/g, '<br>')
     .replaceAll(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;')
-
-  return <div className="content">
-    <div className="field">
-      <p><strong>Console log</strong></p>
-      <div
-        className="has-text-danger-bold"
-        style={{
-          fontFamily: "var(--bulma-code-family)",
-          fontSize: "var(--bulma-pre-font-size)",
-          backgroundColor: "var(--bulma-pre-background)",
-          padding: "var(--bulma-pre-padding)",
-        }}>
-        <SanitizeHTML html={errorMessage}>
-        </SanitizeHTML>
-      </div>
+  return (
+    <div
+      className="has-text-danger-bold"
+      style={{
+        fontFamily: "var(--bulma-code-family)",
+        fontSize: "var(--bulma-pre-font-size)",
+        backgroundColor: "var(--bulma-pre-background)",
+        padding: "var(--bulma-pre-padding)",
+      }}>
+      <SanitizeHTML html={html}>
+      </SanitizeHTML>
     </div>
-  </div>
+  )
 }
