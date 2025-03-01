e1d18652dea1d8f865ea45fbde33d6b2c09f779e
sboursault <sboursault@protonmail.com>
2025-03-01 15:12:14 +0100

    fix ReferenceError: window is not defined

--- src/app/_components/theme-switch.tsx
+++ src/app/_components/theme-switch.tsx
@@ -1,32 +1,31 @@
-"use client"
-import { useState, useEffect } from 'react'
-import { useTheme } from 'next-themes'
+'use client';
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
 
 const ThemeSwitch = () => {
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    const preferedTheme = darkThemeMq.matches ? 'dark' : 'light';
-    const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
-    if (theme == 'system'){
-        setTheme(preferedTheme)
-      }
-    
-  }, [preferedTheme, setTheme, theme])
+    if (theme == 'system') {
+      const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+      const preferedTheme = darkThemeMq.matches ? 'dark' : 'light';
+      setTheme(preferedTheme);
+    }
+  }, [setTheme, theme]);
 
   if (!mounted) {
-    return null
+    return null;
   }
 
   return (
-    <select value={theme} onChange={e => setTheme(e.target.value)}>
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
       <option value="dark">Dark</option>
       <option value="light">Light</option>
     </select>
-  )
-}
+  );
+};
 
-export default ThemeSwitch
+export default ThemeSwitch;
